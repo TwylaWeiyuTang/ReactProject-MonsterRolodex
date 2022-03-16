cd2c405dd105c2bc7adaed796894ab2071ea7ae8
Weiyu Tang <twyla9876@outlook.com>
2022-03-16 17:53:19 +0000

    implemented the search function to filter the data out

--- WebDevelopment/monsters-rolodex/src/App.js
+++ WebDevelopment/monsters-rolodex/src/App.js
@@ -6,7 +6,8 @@ import CardListComponent from './components/card-list/CardListComponent';
    constructor() {
      super();
      this.state = {
-       monsters: [] // initial state
+       monsters: [], // initial state
+       searchField: ''
      };
    } // state only lives in one location and it passes down as props
 
@@ -20,9 +21,19 @@ import CardListComponent from './components/card-list/CardListComponent';
    }
 
    render() {
-     return (
+     const {monsters, searchField } = this.state; // this is destructure
+     // destructure is pulling properties off an object and set them to the constants
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    // includes is trying to see if the monster name string in the monster state includes the searchField string we just passed in
+     
+      return (
        <div className='App'>
-         <CardListComponent monsters={this.state.monsters} /> 
+         <input type='search' placeholder='search monsters' 
+          onChange={e => this.setState({searchField: e.target.value})} />
+          {/* We are defining a function, but we are not running it */}
+         {/* e is the change that is happening, target is where this change is happening, value is the change itself */}
+         {/* if we want to see or do sth with our updated state, we need to do it inside, with a second argument function following the first one*/}
+         <CardListComponent monsters={filteredMonsters} /> 
 
          {/* the children of a prop is anything in between the two brackets of the component */}
          {/* the prop is the thing passed in after the name of the component */}
